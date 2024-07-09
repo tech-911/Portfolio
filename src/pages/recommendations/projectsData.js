3128b76b3debd5b72da50782fdf0ba3405937474
tech-911 <babatundeolatunji74@gmail.com>
2024-07-09 11:52:59 +0100

    added project in gallary

--- src/pages/recommendations/projectsData.js
+++ src/pages/recommendations/projectsData.js
@@ -7,8 +7,21 @@ import halal from "../../assets/gallary-logos/halal.png";
 import zuri from "../../assets/gallary-logos/zuri.jpg";
 import renager from "../../assets/gallary-logos/renager.png";
 import davis from "../../assets/recommend1.png";
+import lendsqr from "../../assets/lendsqr.png";
 
 const projectGallary = [
+  {
+    heading: "Lendsqr",
+    heading2: "Build company's MVP",
+    value: "Frontend Developer",
+    value2: "Building a modular, interactive, pixel perfect UI application.",
+    timeLine: "2024 - Date",
+    link: "<a href='http://babatunde-eric-olatunji-lendsqr-fe-test.vercel.app/'>Lendsqr.com</a>",
+    id: 0,
+    borderBottom: true,
+    tools: "Next.js",
+    logo: lendsqr,
+  },
   {
     heading: "Kuadratik Tech. Ltd.",
     heading2: "Build company's MVP",
@@ -17,9 +30,9 @@ const projectGallary = [
       "Building a modular, interactive, pixel perfect UI application that enables school managements streamline routine school workflows to focus on what matters most - students!",
     timeLine: "2024 - Date",
     link: "<a href='https://kuadratic-dashboard-clone.vercel.app'>Kuadratik.com</a>",
-    id: 0,
+    id: 1,
     borderBottom: true,
-    tools: "Tools Used: Next.js",
+    tools: "Next.js",
     logo: Kuadratik,
   },
   // {
@@ -32,7 +45,7 @@ const projectGallary = [
   //   link: "<a href='http://137.184.47.182:3000/home'>Payaccess.com</a>",
   //   id: 0,
   //   borderBottom: true,
-  //   tools: "Tools Used: Next.js",
+  //   tools: "Next.js",
   //   logo: payaccess,
   // },
   {
@@ -43,9 +56,9 @@ const projectGallary = [
       "Worked with a team in the development of a qur'anic media application with different language options for lectures and with lecturers playlist.",
     timeLine: "2023",
     link: "<a href='https://dn-demo.vercel.app/'>DN-demo.com</a>",
-    id: 1,
+    id: 2,
     borderBottom: true,
-    tools: "Tools Used: React.js",
+    tools: "React.js",
     logo: dN,
   },
   {
@@ -56,9 +69,9 @@ const projectGallary = [
       "In-school transportation application (Done with React.js and express.js).",
     timeLine: "2023",
     link: "<a href='https://ticket-express-web.vercel.app/'>fut-ics.com</a>",
-    id: 2,
+    id: 3,
     borderBottom: true,
-    tools: "Tools Used: react.js & express.js",
+    tools: "react.js & express.js",
     logo: futIts,
   },
   {
@@ -69,9 +82,9 @@ const projectGallary = [
       "Transport solution Tools used for state (Done with React.js and express.js).",
     timeLine: "2023",
     link: "<a href='https://nsta-app-web.vercel.app/'>NSTA-app.com</a>",
-    id: 3,
+    id: 4,
     borderBottom: true,
-    tools: "Tools Used: react.js & express.js",
+    tools: "react.js & express.js",
     logo: nsta,
   },
   {
@@ -81,9 +94,9 @@ const projectGallary = [
     value2: "Portfolio application and book shopping site.",
     timeLine: "2023",
     link: "<a href='https://daviesbamigboye.vercel.app/'>daviesbamigboye.com</a>",
-    id: 4,
+    id: 5,
     borderBottom: true,
-    tools: "Tools Used: react.js",
+    tools: "react.js",
     logo: davis,
   },
   {
@@ -93,9 +106,9 @@ const projectGallary = [
     value2: "A dating application (React.js and Express.js).",
     timeLine: "2023",
     link: "<a href='https://halal-three.vercel.app/'>Halal.com</a>.",
-    id: 5,
+    id: 6,
     borderBottom: true,
-    tools: "Tools Used: react.js & express.js",
+    tools: "react.js & express.js",
     logo: halal,
   },
   {
@@ -106,21 +119,21 @@ const projectGallary = [
       "I was involved in interpreting figma designs into pixel perfect interactive pages for both desktop and mobile screens.",
     timeLine: "2022",
     link: "<a href='https://drive.google.com/file/d/16c_Qh44EWkZ8NRWyNOgChoRdDu-HkCEG/view?usp=drive_link'>Zuri Certified</a>.",
-    id: 6,
+    id: 7,
     borderBottom: true,
-    tools: "Tools Used: react.js",
+    tools: "react.js",
     logo: zuri,
   },
   {
-    heading: "Ultimate Techie Internship",
+    heading: "Project Upgrade",
     heading2: "Renager",
     value: "Intern",
     value2: "Developed a rentals manager application UI.",
     timeLine: "2022",
     link: "<a href='https://renager-mayorhack.vercel.app/'>renager.com</a>",
-    id: 7,
+    id: 8,
     borderBottom: true,
-    tools: "Tools Used: react.js",
+    tools: "react.js",
     logo: renager,
   },
 ];
