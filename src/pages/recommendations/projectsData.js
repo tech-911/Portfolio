c9ed941bd4ada6635128d804b10062ab52817a2d
tech-911 <babatundeolatunji74@gmail.com>
2025-08-20 07:12:47 +0200

    updated profile

--- src/pages/recommendations/projectsData.js
+++ src/pages/recommendations/projectsData.js
@@ -39,19 +39,6 @@ const projectGallary = [
     tools: "Next.js",
     logo: Kuadratik,
   },
-  // {
-  //   heading: "Vladen Tech. Ltd.",
-  //   heading2: "Build company's MVP",
-  //   value: "Frontend Developer",
-  //   value2:
-  //     "Worked on an integrated retail management payment solution that gives businesses and Businesses a 360-degree view of their business and payment collections as well as a platform to create and integrate multiple payment collection options.",
-  //   timeLine: "2023 - Date",
-  //   link: "<a href='http://137.184.47.182:3000/home'>Payaccess.com</a>",
-  //   id: 0,
-  //   borderBottom: true,
-  //   tools: "Next.js",
-  //   logo: payaccess,
-  // },
   {
     heading: "Dawah Nigeria",
     heading2: "DN-Demo",
@@ -187,6 +174,19 @@ const projectGallary = [
     borderBottom: true,
     tools: "Next.js",
     logo: cardInfra,
+  },
+  {
+    heading: "Vladen Tech. Ltd.",
+    heading2: "Build company's MVP",
+    value: "Frontend Developer",
+    value2:
+      "Worked on an integrated retail management payment solution that gives businesses and Businesses a 360-degree view of their business and payment collections as well as a platform to create and integrate multiple payment collection options.",
+    timeLine: "2023 - Date",
+    link: "<a href='https://business-frontend-my2egryd6-payacess.vercel.app/home'>Payaccess.com</a>",
+    id: 13,
+    borderBottom: true,
+    tools: "Next.js",
+    logo: payaccess,
   },
 ];
 
